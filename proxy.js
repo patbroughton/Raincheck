3da0616ff87177fcadbf17efcd05631f038928b1
Pat Broughton <55497480+patbroughton@users.noreply.github.com>
2020-03-25 13:51:22 -0400

    Renamed 'script.js' to 'index.js'.

--- proxy.js
+++ proxy.js
@@ -8,11 +8,14 @@ app.use((req, res, next) => {
 });
 
 app.get('/weather', async (req, res) => {
- 
-  const api_url = 'https://api.darksky.net/forecast/84bf8dba2301b12fb20120c993d6b0d6/42.3601,-71.0589';
-  const response  = await fetch(api_url);
-  const json = await response.json();
-  res.json(json);
+  try{
+    const api_url = 'https://api.darksky.net/forecast/84bf8dba2301b12fb20120c993d6b0d6/42.3601,-71.0589';
+    const response  = await fetch(api_url);
+    const json = await response.json();
+    res.json(json);
+  } catch (error) {
+      console.error(error);
+    }
 });
 
 const PORT = process.env.PORT || 3000;
--- public/index.js
+++ public/index.js
@@ -14,12 +14,16 @@ const appendWeatherToDOM = (weather) => {
 const fetchWeather = async () => {
     let weather;
     const api_url = `weather`;
-    const response = await fetch(api_url);
-    const json = await response.json();
-    weather = json.currently;
-    console.log(weather);
-    // append to DOM
-    appendWeatherToDOM(weather);
+    try{
+        const response = await fetch(api_url);
+        const json = await response.json();
+        weather = json.currently;
+        console.log(weather);
+        // append to DOM
+        appendWeatherToDOM(weather);
+    } catch (error) {
+        console.error(error);
+      }
 };
 
 fetchWeather();
