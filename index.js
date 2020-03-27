f8d62081a26dfd68f7251a8ccb233ad6cd152b69
Pat Broughton <55497480+patbroughton@users.noreply.github.com>
2020-03-27 19:36:17 -0400

    Re-organized client functions to be more modular.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-var lat, lon;
+let lat, lon, numDays;
+
+class DailyData {
+    constructor() {
+      this.day = 0;
+      this.year = 0;
+      this.month = 0;
+      this.date = 0;
+      this.hour = 0;
+      this.minute = 0;
+    }
+}
+dailyDataArray = [];
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+
+app.use(express.json());
 
 app.get('/weather', async (req, res) => {
   try{
@@ -14,8 +28,9 @@ app.get('/weather', async (req, res) => {
     console.log(`Lon is ${lon}`);
     //const api_url = `https://api.darksky.net/forecast/84bf8dba2301b12fb20120c993d6b0d6/${lat},${lon},${yesterday}`;
     // TODO: Don't forget to handle time zone
-    const numDays = 9;
-    const api_url = `https://api.weather.gov/stations/kbkl/observations?start=${year[numDays]}-${month[numDays]}-${date[numDays]}T${hour[numDays]}%3A${minute[numDays]}%3A00-05%3A00&end=${year[0]}-${month[0]}-${date[0]}T${hour[0]}%3A${minute[0]}%3A00-05%3A00`;
+    const start_timestamp = `${dailyDataArray[numDays].year}-${dailyDataArray[numDays].month}-${dailyDataArray[numDays].date}T${dailyDataArray[numDays].hour}%3A${dailyDataArray[numDays].minute}`;
+    const end_timestamp = `${dailyDataArray[0].year}-${dailyDataArray[0].month}-${dailyDataArray[0].date}T${dailyDataArray[0].hour}%3A${dailyDataArray[0].minute}`;
+    const api_url = `https://api.weather.gov/stations/kbkl/observations?start=${start_timestamp}%3A00-05%3A00&end=${end_timestamp}%3A00-05%3A00`;
     const response  = await fetch(api_url);
     const json = await response.json();
     res.json(json);
@@ -24,27 +39,21 @@ app.get('/weather', async (req, res) => {
     }
 });
 
-app.use(express.json());
-
 app.post('/api', (request, response) => {
   console.log('Request received...');
   console.log(request.body);
   lat = request.body.lat;
   lon = request.body.lon;
-  year = request.body.year;
-  month = request.body.month;
-  date = request.body.date;
-  hour = request.body.hour;
-  minute = request.body.minute;
+  numDays = request.body.numDays;
+  for (i=0; i<numDays+1; i++){
+    dailyDataArray[i] = request.body.dailyDataArray[i]
+  }
   response.json({
     status: 'success',
     latitude: lat,
     longitude: lon,
-    year: year,
-    month: month,
-    date: date,
-    hour: hour,
-    minute: minute
+    numDays: numDays,
+    dailyDataArray: dailyDataArray
   });
 });
 
--- public/client.js
+++ public/client.js
@@ -1,31 +1,34 @@
+const NUM_DAYS_MAX = 30;
+let numDays = 3;
+let lat, lon, weather;
+const now = new Date()
 
-const now = new Date()  
-
-var day = [];
-var year = [];
-var month = [];
-var date = [];
-var hour = [];
-var minute = [];
-for (i=0; i<11; i++){
-    day[i] = new Date(now - ((86400*i) * 1000));
-    year[i] = day[i].getFullYear();
-    month[i] = day[i].getMonth() + 1;
-    date[i] = day[i].getDate();
-    hour[i] = day[i].getHours();
-    minute[i] = day[i].getMinutes();
+class DailyData {
+    constructor() {
+      this.day = 0;
+      this.year = 0;
+      this.month = 0;
+      this.date = 0;
+      this.hour = 0;
+      this.minute = 0;
+    }
 }
-
-console.log(`Date: ${year[0]}-${month[0]}-${date[0]}`);
-console.log(`Time: ${hour[0]}:${minute[0]}`);
+dailyDataArray = [];
+for (i=0; i< NUM_DAYS_MAX+1; i++){
+    dailyDataArray[i] = new DailyData();
+    dailyDataArray[i].day = new Date(now - ((86400*i) * 1000));
+    dailyDataArray[i].year = dailyDataArray[i].day.getFullYear();
+    dailyDataArray[i].month = dailyDataArray[i].day.getMonth() + 1;
+    dailyDataArray[i].date = dailyDataArray[i].day.getDate();
+    dailyDataArray[i].hour = dailyDataArray[i].day.getHours();
+    dailyDataArray[i].minute = dailyDataArray[i].day.getMinutes();
+}
+console.log(`Daily Data Array: ${dailyDataArray}`);
 console.log(`Today: ${now}`);
 
 
 const createWeatherLi = (weather) => {
     const li = document.createElement('li');
-    // add user details to `li`
-    //li.textContent = `${weather.data[0].precipIntensity}`;
-    //li.textContent = `${weather.features[0].properties.temperature.value}`;
     li.textContent = `${weather}`;
     return li;
 };
@@ -36,58 +39,76 @@ const appendWeatherToDOM = (weather) => {
 };
 
 const fetchWeather = async () => {
-    let weather;
+    console.log('fetchWeather starting');
+    //Send location, requested number of days to report, and date-time info to server
+    const data = {lat, lon, numDays, dailyDataArray};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) 
+    };
+    //Await the server's response
+    console.log(`sending lat data of ${lat} and lon data of ${lon} to server`);
+    let response = await fetch('/api', options);
+    response = await response.json();
+    console.log(response);
+    //Request weather data from server
     const api_url = `weather`;
     try{
         const response = await fetch(api_url);
+        //Await the server's response
         const json = await response.json();
-        //weather = json.daily;
         weather = json;
         console.log(weather);
-        // append to DOM
-        var rainTotalMeters = 0;
-        for (i=0; i<weather.features.length; i++){
-            appendWeatherToDOM(weather.features[i].properties.precipitationLastHour.value);
-            rainTotalMeters += weather.features[i].properties.precipitationLastHour.value;
-        }
-        const rainTotalInches = (rainTotalMeters * 1000) / 25.4;
-        const averageRainTotalInches = (rainTotalInches / weather.features.length) * 216;
-        console.log(`Rain total (inches): ${rainTotalInches}`);
-        console.log(`Average rain total (inches): ${averageRainTotalInches}`);
-        
+        //Calculate total rainfall and diplay it
+        const rainTotal = calculateRainfall();
+        appendWeatherToDOM(Math.round((rainTotal + Number.EPSILON) * 100) / 100);
     } catch (error) {
         console.error(error);
       }
-};
-
-function geolocate(resolve, reject) {  
-    if ('geolocation' in navigator) {
-        console.log('geolocation available');
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position.coords);
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            document.getElementById('latitude').textContent = lat.toFixed(2);
-            document.getElementById('longitude').textContent = lon.toFixed(2);
-            const data = {lat, lon, year, month, date, hour, minute};
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data) 
-            };
-            fetch('/api', options)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                fetchWeather();
-            });
-        });                  
-    } else {
-        console.log('geolocation not available');
-    }       
+    console.log('fetchWeather finished');
 }
-geolocate();
+
+const calculateRainfall = () => {
+    let rainTotalMeters = 0;
+    for (i=0; i<weather.features.length; i++){
+        rainTotalMeters += weather.features[i].properties.precipitationLastHour.value;
+    }
+    const rainTotalInches = (rainTotalMeters * 1000) / 25.4;
+    const averageRainTotalInches = (rainTotalInches / weather.features.length) * 24 * numDays;
+    console.log(`Rain total (inches): ${rainTotalInches}`);
+    console.log(`Average rain total (inches): ${averageRainTotalInches}`);
+    return averageRainTotalInches
+}
+
+function geolocate() {
+    return new Promise((resolve, reject) => {  
+        console.log('geolocate starting');
+        //Check for geolocation capability
+        if ('geolocation' in navigator) {
+            console.log('geolocation available');
+            //Send latitude and longitude to server
+            navigator.geolocation.getCurrentPosition(position => {
+                console.log(position.coords);
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+                document.getElementById('latitude').textContent = lat.toFixed(2);
+                document.getElementById('longitude').textContent = lon.toFixed(2);
+                console.log('Callback finished');
+                resolve();
+            });
+            console.log('geolocate finished');                 
+        } else {
+            console.log('geolocation not available');
+            reject();
+            console.log('geolocate finished');
+        }
+    });       
+}
+
+geolocate().then(fetchWeather);
+
 
 
