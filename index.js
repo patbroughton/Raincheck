60f6f121e7baf7835a536ba060803d763d35c8f1
Pat Broughton <55497480+patbroughton@users.noreply.github.com>
2020-03-26 21:30:10 -0400

    Started fetching rainfall data from weather.gov in order to be able to get multiple days of data with a single api call. Averaging the data seems to work when comparing to wunderground historical data. Need to incorporate geolocation into this, including finding nearby weather stations to use.

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.get('/weather', async (req, res) => {
   try{
     console.log(`Lat is ${lat}`);
     console.log(`Lon is ${lon}`);
-    const api_url = `https://api.darksky.net/forecast/84bf8dba2301b12fb20120c993d6b0d6/${lat},${lon}`;
+    console.log(`Seconds since epoch = ${secondsSinceEpoch}`)
+    const yesterday = secondsSinceEpoch - (60 * 60 * 24);
+    console.log(`Seconds 24 hours ago: ${yesterday}`);
+    //const api_url = `https://api.darksky.net/forecast/84bf8dba2301b12fb20120c993d6b0d6/${lat},${lon},${yesterday}`;
+    const api_url = 'https://api.weather.gov/stations/kbkl/observations?start=2020-03-17T00%3A00%3A00-05%3A00&end=2020-03-21T00%3A00%3A00-05%3A00';
     const response  = await fetch(api_url);
     const json = await response.json();
     res.json(json);
@@ -28,6 +32,7 @@ app.post('/api', (request, response) => {
   console.log(request.body);
   lat = request.body.lat;
   lon = request.body.lon;
+  secondsSinceEpoch = request.body.secondsSinceEpoch;
   response.json({
     status: 'success',
     latitude: lat,
--- public/client.js
+++ public/client.js
@@ -1,8 +1,13 @@
 
+const now = new Date()  
+const secondsSinceEpoch = Math.round(now.getTime() / 1000) 
+
 const createWeatherLi = (weather) => {
     const li = document.createElement('li');
     // add user details to `li`
-    li.textContent = `${weather.summary}`;
+    //li.textContent = `${weather.data[0].precipIntensity}`;
+    //li.textContent = `${weather.features[0].properties.temperature.value}`;
+    li.textContent = `${weather}`;
     return li;
 };
 
@@ -17,10 +22,20 @@ const fetchWeather = async () => {
     try{
         const response = await fetch(api_url);
         const json = await response.json();
-        weather = json.currently;
+        //weather = json.daily;
+        weather = json;
         console.log(weather);
         // append to DOM
-        appendWeatherToDOM(weather);
+        var rainTotalMeters = 0;
+        for (i=0; i<weather.features.length; i++){
+            appendWeatherToDOM(weather.features[i].properties.precipitationLastHour.value);
+            rainTotalMeters += weather.features[i].properties.precipitationLastHour.value;
+        }
+        const rainTotalInches = (rainTotalMeters * 1000) / 25.4;
+        const averageRainTotalInches = (rainTotalInches / weather.features.length) * 96;
+        console.log(`Rain total (inches): ${rainTotalInches}`);
+        console.log(`Average rain total (inches): ${averageRainTotalInches}`);
+        
     } catch (error) {
         console.error(error);
       }
@@ -35,7 +50,7 @@ function geolocate(resolve, reject) {
             const lon = position.coords.longitude;
             document.getElementById('latitude').textContent = lat.toFixed(2);
             document.getElementById('longitude').textContent = lon.toFixed(2);
-            const data = {lat, lon};
+            const data = {lat, lon, secondsSinceEpoch};
             const options = {
                 method: 'POST',
                 headers: {
