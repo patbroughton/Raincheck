08b8cc6064445d78829d2157f72732f0e93a431f
Pat Broughton <55497480+patbroughton@users.noreply.github.com>
2020-03-30 21:38:49 -0400

    Removed timezone offset for now because it creates invalid hour values (ex: '25'.

--- public/client.js
+++ public/client.js
@@ -23,7 +23,8 @@ for (i=0; i< NUM_DAYS_MAX+1; i++){
     dailyDataArray[i].year = dailyDataArray[i].day.getFullYear();
     dailyDataArray[i].month = dailyDataArray[i].day.getMonth() + 1;
     dailyDataArray[i].date = dailyDataArray[i].day.getDate();
-    dailyDataArray[i].hour = dailyDataArray[i].day.getHours() + (timezoneOffset / 60);
+    //dailyDataArray[i].hour = dailyDataArray[i].day.getHours() + (timezoneOffset / 60);
+    dailyDataArray[i].hour = dailyDataArray[i].day.getHours();
     dailyDataArray[i].minute = dailyDataArray[i].day.getMinutes();
 }
 console.log(`Daily Data Array: ${dailyDataArray}`);
@@ -140,6 +141,44 @@ function geolocate() {
         }
     });       
 }
+
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
 
 geolocate().then(fetchWeather);
 
